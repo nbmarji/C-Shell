a6e65bf31a50663ba718553fb9b7c7416b27d999
nbmarji <59606014+nbmarji@users.noreply.github.com>
2020-11-10 23:49:19 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,4 @@
-/* 
-Name: Nora Marji
-Class: CS 344
-Date: November 4th, 2020
-Description: Smallsh is a shell written in C.  It includes the following features:
-1. Provides a prompt for running commands
-2. Handles blank lines and comments, which are lines beginning with the # character
-3. Provides expansion for the variable $$
-4. Executes 3 commands exit, cd, and status via code built into the shell
-5. Executes other commands by creating new processes using a function from the exec family of functions
-6. Supports input and output redirection
-7. Supports running commands in foreground and background processes
-8. Implements custom handlers for 2 signals, SIGINT and SIGTSTP
-*/
+
 
 #include<stdio.h>
 #include<string.h>
